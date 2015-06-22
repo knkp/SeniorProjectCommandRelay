61bfddd2e82a5008a3fd7d366de24a254374992f
knkp <knkp934@gmail.com>
2015-06-22 11:33:39 +0000

    finally got it working, user can enter an id on the tivaC, it be sent to the pi, the pi will then check if it already exists, if it does, for now, it sends a command to the arduino to open the associated cabinet for 5 seconds and then closes it again. The final step for this aspect of the system is to integrate all this into the state machine, after that it's just a matter of buildig the management utility and integrating that into the command_relay, or really the RPSS_Pi_Manager

--- RPSS_Pi_Manager.py
+++ RPSS_Pi_Manager.py
@@ -1,6 +1,6 @@
 import serial
 import socket
-
+from time import sleep
 
 CABINET_ID = 0
 
@@ -8,16 +8,17 @@ CABINET_ID = 0
 def checkIfInDatabase(ID, file):
         with open(file) as DB_FILE:
                 for line in DB_FILE:
-                        splitLine = line.split('|')
-                        if splitLine[0] is ID:
+                        splitLine = line.split()
+			print("current value in file is: "+splitLine[0])
+			print("current ID value is: "+ID)
+                        if splitLine[0] == ID:
                                 return 1
-                        else:
-                                return 0
+		return 0
 
 def register(ID, file, CB_ID):
         DB = open(file,'a+')
 	CB_ID, C_ID_string = getNextCabinet(CABINET_ID)
-        DB.write(ID + '|' + C_ID_string+'\n')
+        DB.write(ID + ' ' + C_ID_string+'\n')
 	return CB_ID
 
 def getNextCabinet(cabinetID):
@@ -27,7 +28,15 @@ def getNextCabinet(cabinetID):
                 cabinetID = 0
         return cabinetID, str(cabinetID)
 
-
+def open_cabinet(cabinetID,arduino):
+	arduino.write('x') #tell the arduino that we are going to send it a cabinet ID to open
+	arduino.read() #wait for acknowledge
+	arduino.write(bytes(cabinetID))  #send id
+	arduino.read() #receive ack
+	sleep(5) #wait for 5 seconds before closing cabinet
+	arduino.write('M') #tell arduino we are going to close a cabinet
+	arduino.write(bytes(cabinetID)) #send id to close
+	arduino.read()
 #print("setting up socket")
 #server_address = ('192.168.0.3',5001)
 
@@ -66,7 +75,7 @@ while 1:
 			print("ID received, ID is: "+id_string);
 			conn1.flush()
 			if checkIfInDatabase(id_string,'./DB_FILE') is 1:
-				open_cabinet()
+				open_cabinet(CABINET_ID, conn2)
 			else:
 				CABINET_ID = register(id_string, './DB_FILE', CABINET_ID)
 
